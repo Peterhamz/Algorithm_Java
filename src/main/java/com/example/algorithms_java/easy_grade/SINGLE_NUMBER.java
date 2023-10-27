18fe21c5d5c1c739d22fc9889dd6d1bdb2739394
peter <peterhamza6@gmail.com>
2023-10-27 11:14:28 +0100

    Solving with Hashmap

--- src/main/java/com/example/algorithms_java/easy_grade/SINGLE_NUMBER.java
+++ src/main/java/com/example/algorithms_java/easy_grade/SINGLE_NUMBER.java
@@ -9,8 +9,8 @@ public class SINGLE_NUMBER {
 
 
     public static void main(String[] args) {
-        int[] test = {4,2,2,1,1};
-        int result = singleNumber(test);
+        int[] test = {4,4,2,1,1};
+        int result = singleNumberHashMap(test);
         System.out.println(result);
     }
 
@@ -19,7 +19,7 @@ public class SINGLE_NUMBER {
 
 
 
-    // Using bitwize Operator
+    // Using bitwise Operator
     public static int singleNumber(int[] nums){
         int result = 0;
         for (int num : nums) {
@@ -58,8 +58,22 @@ public class SINGLE_NUMBER {
 
 
     // Using HashMap
-
     public static int singleNumberHashMap(int[] nums){
+        // Create a HashMap to store the count of each element.
+       HashMap<Integer, Integer> storeNum = new HashMap<>();
+        //default variable for the result
+       int result = 0;
+       // Store in the nums Items in the hashmap
+        for (int num : nums){
+            storeNum.put(num, storeNum.getOrDefault(num,0) + 1);
+        }
+        // iterate through the map and print the value
+        for (Map.Entry<Integer, Integer> number : storeNum.entrySet()){
+            if (number.getValue() == 1){
+                result = number.getKey();
+            }
+        }
 
+        return result;
     }
 }
