cc7dc55766d4ee6c08d300f671defca97d2e86f2
peter <peterhamza6@gmail.com>
2023-10-30 14:57:18 +0100

    plus number Complete

--- src/main/java/com/example/algorithms_java/easy_grade/Plus_One.java
+++ src/main/java/com/example/algorithms_java/easy_grade/Plus_One.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Plus_One {
     public static void main(String[] args) {
         int[] digits = {9,8,7,6,5,4,3,2,1,0};
-        int[] result = plusOne(digits);
+        int[] result = plusOneNumber(digits);
         System.out.println(Arrays.toString(result));
 
     }
@@ -46,4 +46,37 @@ public class Plus_One {
 
         return resultArray;
     }
+
+    // Another Solution
+    public static int[] plusOneNumber(int[] digits){
+
+
+        int n = digits.length;
+
+
+
+        // Iterate from the rightmost digit to the left
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+
+
+                // If the digit is less than 9, just increment it by 1 and return
+                digits[i]++;
+                return digits;
+            } else {
+
+
+                // If the digit is 9, set it to 0 and continue to the next digit
+                digits[i] = 0;
+            }
+        }
+
+        // If we reached this point, it means we have a carry to the most significant digit
+        // In this case, we need to add a new digit (1) at the beginning of the array
+        int[] result = new int[n + 1];
+        result[0] = 1;
+
+        return result;
+    }
 }
+
