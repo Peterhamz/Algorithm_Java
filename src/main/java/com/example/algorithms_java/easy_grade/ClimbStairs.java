a26462cf1fd8d31384d09ad131b9dedbb253d488
peter <peterhamza6@gmail.com>
2023-11-15 12:01:10 +0100

    Using HashMap for ClimbStairs

--- src/main/java/com/example/algorithms_java/easy_grade/ClimbStairs.java
+++ src/main/java/com/example/algorithms_java/easy_grade/ClimbStairs.java
@@ -1,9 +1,15 @@
 package com.example.algorithms_java.easy_grade;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClimbStairs {
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(3));
+       // System.out.println(climbStairs(2));
+
+        int result = climbingStairsWithHashMap(5);
+        System.out.println("Distinct ways to climb stairs: " + result);
     }
 
     public static int climbStairs(int n) {
@@ -18,4 +24,32 @@ public class ClimbStairs {
 
         return dp[n];
     }
-}
+
+    public static int climbingStairsWithHashMap(int n){
+
+            // Create a HashMap for memoization
+            Map<Integer, Integer> memo = new HashMap<>();
+            return climbStairsHelper(n, memo);
+        }
+
+        private static int climbStairsHelper(int n, Map<Integer, Integer> memo) {
+            // Base cases
+            if (n <= 1) {
+                return 1;
+            }
+
+            // Check if the result for 'n' is already memoized
+            if (memo.containsKey(n)) {
+                return memo.get(n);
+            }
+
+            // Calculate the result for 'n' and memoize it
+            int ways = climbStairsHelper(n - 1, memo) + climbStairsHelper(n - 2, memo);
+            memo.put(n, ways);
+            System.out.println(ways);
+
+            return ways;
+        }
+
+    }
+
